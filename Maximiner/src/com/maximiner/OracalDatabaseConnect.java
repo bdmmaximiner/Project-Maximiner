91bca0c987bbe04225d5281ef8a4d9fc1b5f23a7
lap <lap@192.168.1.9>
2018-08-13 11:55:18 -0700

    Oracle jdbc connection added

---
+++ Maximiner/src/com/maximiner/OracalDatabaseConnect.java
@@ -0,0 +1,21 @@
+package com.maximiner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OracalDatabaseConnect {
+
+	public Connection Connect() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
+
+	}
+
+	public static int rowCount(ResultSet rs) throws SQLException {
+		rs.last();
+		return rs.getRow();
+	}
+
+}
