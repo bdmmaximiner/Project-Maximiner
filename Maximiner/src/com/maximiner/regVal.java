6332dc226aec753a5181585c9fca907555bf9c77
Admin <Admin@192.168.1.34>
2018-08-13 03:14:17 -0700

    deepa

--- Maximiner/src/com/maximiner/regVal.java
+++ Maximiner/src/com/maximiner/regVal.java
@@ -1,10 +1,8 @@
 package com.maximiner;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.Servlet;
