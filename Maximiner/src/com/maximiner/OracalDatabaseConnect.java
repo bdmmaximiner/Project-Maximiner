95ece06deff33e10676891794f895efe457795ad
lap <lap@192.168.1.9>
2018-08-14 12:12:16 -0700

    sql update

--- Maximiner/src/com/maximiner/OracalDatabaseConnect.java
+++ Maximiner/src/com/maximiner/OracalDatabaseConnect.java
@@ -10,7 +10,6 @@ public class OracalDatabaseConnect {
 	public Connection Connect() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
-
 	}
 
 	public static int rowCount(ResultSet rs) throws SQLException {
