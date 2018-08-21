02f9be4e85e26d67223a734575a37870c2f7d692
Admin <Admin@Admin-PC>
2018-08-21 03:34:45 -0700

    # WARNING: head commit changed in the meantime
    work

--- Maximiner/WebContent/js/javascript.js
+++ Maximiner/WebContent/js/javascript.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 });
 
 function onlyAlphabets() {
-	var regex = new RegExp("[a-zA-Z]+\\.?");
+	var regex = ("[a-zA-Z]+\\.?");
 	var key = String.fromCharCode(!event.charCode ? event.which
 			: event.charCode);
 	if (!regex.test(key)) {
@@ -16,5 +16,4 @@ function onlyAlphabets() {
 		document.getElementById("firstnameInvalid").style.visibility = "hidden";
 		$("#firstnameInvalid").text("");
 	}
-
 }
