eebd75152d5c059393175b59c39ae6a04e1438c2
lap <lap@lap-PC>
2018-10-04 14:58:36 +0530

    urel

--- HRPortal/src/com/maximiner/OracalDatabaseConnect.java
+++ HRPortal/src/com/maximiner/OracalDatabaseConnect.java
@@ -10,8 +10,8 @@ public class OracalDatabaseConnect {
 	public Connection Connect() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection(
-				"jdbc:oracle:thin:@myoracle.cupgnhng2n9m.ap-south-1.rds.amazonaws.com:1521:ORCL", "bhaskar",
-				"123456789!");
+				"jdbc:oracle:thin:@oracle.ccr68wkfvwyr.ap-south-1.rds.amazonaws.com:1521:ORCL", "maximiner",
+				"Maximiner_2018");
 	}
 
 	public static int rowCount(ResultSet rs) throws SQLException {
